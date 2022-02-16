c42d8f7e390c7fcb8589381706da27d1e7bd25f5
Anja Greifenberg <anja.gberg@gmx.de>
2022-02-16 15:04:17 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do |restaurant|
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: %w(chinese italian japanese french belgian).sample,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  )
+  puts "Restaurant #{restaurant} done"
+end
+
+puts 'All Restaurants created'
